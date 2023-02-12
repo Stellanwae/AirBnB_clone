b391334c67d2a796d78e7ea27ff9f017901469d1
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-02-12 18:14:54 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""class defining User"""
+from models.base_model import BaseModel
 
+
+class User(BaseModel):
+  email= ""
+  password = ""
+  first_name = ""
+  last_name = ""
+  
