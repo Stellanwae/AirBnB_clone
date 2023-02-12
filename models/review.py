a9faabdf7609097062de28e62330002e432d3db9
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-02-12 17:47:32 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
 
+"""Is the review class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
