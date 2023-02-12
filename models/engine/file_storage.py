77998d168422de0db69859d7d3954260726d1eb5
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-02-12 20:39:00 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,44 @@
+#!/usr/bin/python3
 
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """class filestorage"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns dict objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set __objects with key"""
+        key = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(key, obj.id)] = obj
+
+    def save(self):
+        """Serialize __objects with JSON"""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
+    def reload(self):
+        """Deserialize the JSON"""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for obj in objdict.values():
+                    class_name = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(class_name)(**o))
+        except FileNotFoundError:
+            return
